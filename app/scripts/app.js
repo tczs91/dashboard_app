e2bb2651188c077196297be02dcb8fad455899db
Crystal Sun <biyuesun@gmail.com>
2016-06-22 14:57:43 -0400

    display already using $http

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,3 +7,33 @@
  *
  * Main module of the application. Define the routing architecture for the app
  */
+var routerApp = angular.module('routerApp', ['ui.router']);
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/root/work');
+    $stateProvider
+    // HOME STATES AND NESTED VIEWS ========================================
+        .state('root', {
+            url: '/root',
+            templateUrl: 'templates/root.html'
+        })
+    
+        .state('root.overview', {
+            url: '/overview',
+            templateUrl: 'templates/overview.html'
+        })
+    
+        .state('root.work', {
+            url: '/work',
+            templateUrl: 'templates/work.html',
+            controller:'workController'
+        })
+    
+        .state('root.producer', {
+            url: '/producer',
+            templateUrl: 'templates/producer.html'
+        })
+        .state('root.contact', {
+            url: '/contact',
+            templateUrl: 'templates/contact.html'
+        })
+});
--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -2,3 +2,17 @@
 // WORK CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Work" section to display items and filter them
+routerApp.controller('workController', function($scope,$http) {
+    $http({
+        method : "GET",
+        //api is to find router folder in server
+        url : "/api/work"
+    }).then(function mySucces(response) {
+        $scope.works = response.data.works;
+    }, function myError(response) {
+       console.log("error");
+    });
+});
+//delete(workid){
+//    $("#workid").css("display","none");
+//}
