f177a5658ec39d375498a87efdd9a301311a6be7
tczs91 <zhang_s@gwmail.gwu.edu>
2016-06-25 15:41:18 -0400

    Fix some bugs

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -5,34 +5,46 @@
 // Make service calls to log the user out of the app
 // Populate the current date that the user log in
 
-dashApp.controller('rootController', ['$scope','getUserService','authenticateService','$state',function($scope, getUserService, authenticateService,$state) {
+dashApp.controller('rootController', ['$state','$scope','getUserService','authenticateService','sessionService',function($state,$scope, getUserService, authenticateService,sessionService) {
     ////TODO check login here
     //if not return to login page
     var loggedFlag = authenticateService.islogged();
     if(loggedFlag) {
+          //call getUser service to retirve user full name and display
+          var fullName = '';
+          var userString = sessionService.getSession('userSession');
+          if(userString) {
+            var userObj = JSON.parse(userString);
+            var userName = userObj.username;
+            console.log(userName);
+          } else {
+                var userName = '';
+          }
+          getUserService.getFullname(userName)
+          .then(function(results) {
+                  fullName = results.data.name;
+                  console.log('your fullname is ' + fullName);
+                  $scope.username = fullName;
+            }, function(error) {})
+              .finally(function() {
+           });
+                  $scope.username = fullName;
+          // $scope.$apply(function() {
+          //     $scope.username = fullName;
+          // });
+          //display current date
+          //we can create a string from myDate or
+          //just use filter to change the format of myDate
+          var myDate = new Date();
+          $scope.currentDate = myDate;
+
+          //when click the button call authentication service to logout the user
+          $scope.logout =function() {
+            authenticateService.logout();
+          }
+          $state.transitionTo('root.work');
     }else {
-        $state.transitionTo('login');
+          $state.transitionTo('login');
     }
-
-    //call getUser service to retirve user full name and display
-    $scope.username = {};
-    getUserService.getUser()
-      .then(function(results) {
-              $scope.username = results.data.name;
-      }, function(error) {})
-        .finally(function() {
-     });
-
-    //display current date
-    //we can create a string from myDate or
-    //just use filter to change the format of myDate
-    var myDate = new Date();
-    $scope.currentDate = myDate;
-
-    //when click the button call authentication service to logout the user
-    $scope.logout =function() {
-      authenticateService.logout();
-    }
-
 }]);
 
