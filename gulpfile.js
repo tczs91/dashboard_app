6dcbf10acc7f760624b2ce24aa4b8e0ad9920db1
tczs91 <zhang_s@gwmail.gwu.edu>
2016-06-29 16:00:42 -0400

    gulp task update 3
    finished the third task which can delete the build folder and the
    minified images in it.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var   sass         = require('gulp-sass');
 var   imagemin     = require('gulp-imagemin');
 var   autoprefixer = require('gulp-autoprefixer');
 var   sourcemaps   = require('gulp-sourcemaps');
-var   del          = require('del');
+const del          = require('del');
 var   changed      = require('gulp-changed');
 var   merge        = require('merge-stream');
 
@@ -22,21 +22,41 @@ gulp.task('styles', function(){
 
 //image minify (Second task)
 gulp.task('imagemin', function(){
+    //since we want to work on three pairs of srcs and dests
+    //we want to split them into three order streams,
+    //otherwise we will confront write after end error
+
+    //minify the app images
     var app = gulp.src('./app/assets/images/src/app/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./app/assets/images/build/app/'));
 
+    //minify the profile images
     var profile = gulp.src('./app/assets/images/src/profile/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./app/assets/images/build/profile/'));
 
+    //minify the work images
     var work = gulp.src('./app/assets/images/src/work/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./app/assets/images/build/work/'));
 
+    //merge the three streams
     return merge(app, profile, work);
 
+
+    // //where to use autoprefixer?
+    // return gulp.src('src/images/src/**/*')
+    // .pipe(sourcemaps.init())
+    // .pipe(autoprefixer())
+    // .pipe(imagemin())
+    // .pipe(sourcemaps.write('.'))
+    // .pipe(gulp.dest('src/images/build/*'));
 });
 
-
-
+//remove the build folder
+gulp.task('clean', function(){
+    return del([
+    'app/assets/images/build/**/*', 'app/assets/images/build'
+  ]);
+});
