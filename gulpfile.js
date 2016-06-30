2da1ab27e74c326bfdcb16a266bb48c54e02fc53
tczs91 <zhang_s@gwmail.gwu.edu>
2016-06-29 22:31:36 -0400

    gulp task update 5
    1. fix imagemin task.
    2. finished watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ var   sourcemaps   = require('gulp-sourcemaps');
 const del          = require('del');
 var   changed      = require('gulp-changed');
 var   merge        = require('merge-stream');
-var stylish = require('jshint-stylish');
+var   stylish      = require('jshint-stylish');
 
 //default task
 //the defualt will be stated by using just 'gulp' in bash
-gulp.task('default', ['styles','imagemin','clean','jshint','watch']);
+gulp.task('default', ['styles','imagemin','jshint','watch']);
 
 //styles task (First task)
 gulp.task('styles', function(){
@@ -23,36 +23,11 @@ gulp.task('styles', function(){
 
 //image minify (Second task)
 gulp.task('imagemin', function(){
-    //since we want to work on three pairs of srcs and dests
-    //we want to split them into three order streams,
-    //otherwise we will confront write after end error
 
-    //minify the app images
-    var app = gulp.src('./app/assets/images/src/app/*')
+    return gulp.src('./app/assets/images/src/**/*.*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./app/assets/images/build/app/'));
+    .pipe(gulp.dest('./app/assets//images/build/'));
 
-    //minify the profile images
-    var profile = gulp.src('./app/assets/images/src/profile/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./app/assets/images/build/profile/'));
-
-    //minify the work images
-    var work = gulp.src('./app/assets/images/src/work/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./app/assets/images/build/work/'));
-
-    //merge the three streams
-    return merge(app, profile, work);
-
-
-    // //where to use autoprefixer?
-    // return gulp.src('src/images/src/**/*')
-    // .pipe(sourcemaps.init())
-    // .pipe(autoprefixer())
-    // .pipe(imagemin())
-    // .pipe(sourcemaps.write('.'))
-    // .pipe(gulp.dest('src/images/build/*'));
 });
 
 //remove the build folder
@@ -70,4 +45,13 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
-//
+//watch the js files, sass files and image files
+gulp.task('watch', function() {
+    gulp.watch('app/scripts/**/*.js', ['jshint']);
+    gulp.watch('app/assets/sass/*.scss', ['styles']);
+    gulp.watch('app/assets/images/src/**/*.*', ['imagemin']);
+    //gulp.watch('app/assets/images/build/**/*.*', ['clean']);
+
+});
+
+
