52fea63cc8f2f1f031a6731b68e5df0cf308b7a0
tczs91 <zhang_s@gwmail.gwu.edu>
2016-07-01 18:53:40 -0400

    Merge remote-tracking branch 'origin/ass5_dashboard_contact'

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -2,3 +2,46 @@
 // CONTACT CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Contact" section for the form
+dashApp.controller('contactController',['$scope','$http','contactService',function($scope,$http,contactService) {
+      contactService.getLocation()
+          .then(function(results) {
+                  $scope.locations = results;
+//                  console.log('locations are ' + $scope.locations);
+            }, function(error) {})
+              .finally(function() {
+           });
+     
+    contactService.getCategory()
+          .then(function(results) {
+                  $scope.categories = results;
+            }, function(error) {})
+              .finally(function() {
+           });
+    
+    $scope.nowtime= new Date();  
+    $scope.rate = 7;
+    $scope.max = 10;
+    $scope.isReadonly = false;
+
+    $scope.hoveringOver = function(value) {
+    $scope.overStar = value;
+    $scope.percent = 100 * (value / $scope.max);
+  };
+    $scope.ratingStates = [
+    {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+    {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+    {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+    {stateOn: 'glyphicon-heart'},
+    {stateOff: 'glyphicon-off'}
+  ];
+    
+    $scope.randomNum=Math.floor(Math.random()*100000000);
+    $scope.priority = function(value) {
+    if(value<4)
+      return "Low";
+    else if(value>=4 && value<7)
+      return "Middle";
+    else
+      return "High";
+  };
+}]);
--- app/scripts/services/getContact.js
+++ app/scripts/services/getContact.js
@@ -1,3 +1,28 @@
 'use strict';
 // GETCONTACT SERVICE
 // Description: Define the contactService that has 2 functionalities: getLocation and getCategory 
+dashApp.service('contactService', ['$http',function($http) {
+      this.getLocation = function() {
+                return $http({
+                method : "GET",
+                //api is to find router folder in server
+                url : "/api/location"
+            }).then(function mySucces(response) {
+            return response.data.locations;
+            }, function myError(response) {
+            console.log("error");
+                });  
+      }
+      this.getCategory = function(){
+            return $http({
+                method : "GET",
+                //api is to find router folder in server
+                url : "/api/category"
+            }).then(function mySucces(response) {
+            return response.data.categories;
+            }, function myError(response) {
+            console.log("error");
+                });  
+      }
+      
+}]);
